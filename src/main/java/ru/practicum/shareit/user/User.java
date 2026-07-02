ad8aaf129c6260a8d272fd6a99ec28a7402cd55f
Evgeniy <gladskih07@yandex.ru>
2026-07-02 23:51:44 +0300

    fix Checkstyle

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -3,8 +3,6 @@ package ru.practicum.shareit.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
