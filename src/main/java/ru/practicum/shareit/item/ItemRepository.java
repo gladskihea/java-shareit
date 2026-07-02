f2bd38c625bab6f2daf904003724994987f1f634
Evgeniy <gladskih07@yandex.ru>
2026-07-02 22:51:59 +0300

    fix

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -6,9 +6,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ItemRepository {
+
     Item save(Item item);
+
     Optional<Item> findById(Long id);
+
     List<Item> findAllByOwnerId(Long ownerId);
+
     Item update(Item item);
+
     List<Item> search(String text);
 }
--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -5,9 +5,14 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import java.util.List;
 
 public interface ItemService {
+
     ItemDto addItem(Long userId, ItemDto itemDto);
+
     ItemDto updateItem(Long userId, Long itemId, ItemDto itemDto);
+
     ItemDto getItemById(Long itemId);
+
     List<ItemDto> getAllItemsByOwner(Long userId);
+
     List<ItemDto> searchItems(String text);
 }
