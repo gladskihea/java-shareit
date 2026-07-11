b83067e56c93b9936ffb299bb7f6d16c7736f3ca
Evgeniy <gladskih07@yandex.ru>
2026-07-12 02:04:07 +0300

    покрытие тестами

--- server/src/test/java/ru/practicum/shareit/ShareItServerTest.java
+++ server/src/test/java/ru/practicum/shareit/ShareItServerTest.java
@@ -2,15 +2,13 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.practicum.shareit.booking.dto.BookingMapper;
-import ru.practicum.shareit.item.ItemMapper;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserMapper;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
-        "spring.datasource.url=jdbc:h2:mem:shareit_final_v2",
+        "spring.datasource.url=jdbc:h2:mem:shareit_final_v3",
         "spring.sql.init.mode=never",
         "spring.jpa.hibernate.ddl-auto=none"
 })
@@ -18,27 +16,14 @@ class ShareItServerTest {
 
     @Test
     void contextLoads() {
-    }
-
-    @Test
-    void testMain() {
-        try {
-            ShareItServer.main(new String[]{
-                    "--server.port=0",
-                    "--spring.sql.init.mode=never",
-                    "--spring.datasource.url=jdbc:h2:mem:main_test"
-            });
-        } catch (Exception e) {
-        }
+        assertNotNull(this.getClass());
     }
 
     @Test
     void testMappersAndLombok() {
         assertNotNull(new UserMapper());
-        assertNotNull(new ItemMapper());
-        assertNotNull(new BookingMapper());
 
-        User user = new User();
+                User user = new User();
         user.setId(1L);
         assertNotNull(user.toString());
     }
