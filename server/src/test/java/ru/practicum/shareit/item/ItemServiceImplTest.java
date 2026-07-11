29536ad487200ea32c0a737c32c3ed0d87c457e5
Evgeniy <gladskih07@yandex.ru>
2026-07-11 13:53:40 +0300

    чекстайл

--- server/src/test/java/ru/practicum/shareit/item/ItemServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/item/ItemServiceImplTest.java
@@ -16,7 +16,6 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.item.model.CommentRepository;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.request.ItemRequestRepository;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserRepository;
