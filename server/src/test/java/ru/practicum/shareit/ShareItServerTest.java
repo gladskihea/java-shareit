b97765ede750266badba48fd20d0a07e0efcea77
Evgeniy <gladskih07@yandex.ru>
2026-07-11 14:53:13 +0300

    покртытие тестами

--- server/src/test/java/ru/practicum/shareit/ShareItServerTest.java
+++ server/src/test/java/ru/practicum/shareit/ShareItServerTest.java
@@ -4,7 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
+        "spring.datasource.url=jdbc:h2:mem:shareit;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.sql.init.mode=always"
+})
 @ActiveProfiles("test")
 class ShareItServerTest {
     @Test
