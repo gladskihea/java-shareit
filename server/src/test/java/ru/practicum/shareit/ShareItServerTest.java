cc9d0967e2ab4980f09f768b87caa6f19ab55463
Evgeniy <gladskih07@yandex.ru>
2026-07-12 01:48:42 +0300

    покрытие тестами

--- server/src/test/java/ru/practicum/shareit/ShareItServerTest.java
+++ server/src/test/java/ru/practicum/shareit/ShareItServerTest.java
@@ -4,14 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
-        "spring.datasource.url=jdbc:h2:mem:shareit_final;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE",
-        "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-        "spring.sql.init.mode=never"
+        "spring.datasource.url=jdbc:h2:mem:shareit_final_check",
+        "spring.sql.init.mode=never",
+        "spring.jpa.hibernate.ddl-auto=none"
 })
 class ShareItServerTest {
     @Test
     void contextLoads() {
-        ShareItServer.main(new String[]{"--server.port=0"});
     }
 }
