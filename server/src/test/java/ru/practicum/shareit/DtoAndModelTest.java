f6cc6c4e58ef504d50a94a53f942af5e14c81d7c
Evgeniy <gladskih07@yandex.ru>
2026-07-12 00:31:56 +0300

    покрытие тестами

--- server/src/test/java/ru/practicum/shareit/DtoAndModelTest.java
+++ server/src/test/java/ru/practicum/shareit/DtoAndModelTest.java
@@ -1,36 +1,114 @@
 package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.booking.Booking;
+import ru.practicum.shareit.booking.BookingStatus;
+import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingOutDto;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Comment;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserDto;
+
 import java.time.LocalDateTime;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class DtoAndModelTest {
+
     @Test
-    void testAllDto() {
-        CommentDto commentDto = new CommentDto(1L, "text", "author", LocalDateTime.now());
-        assertEquals("text", commentDto.getText());
+    void forceCoverageForAllModelsAndDtos() {
+        LocalDateTime now = LocalDateTime.now();
 
-        ItemDto itemDto = new ItemDto();
-        itemDto.setName("item");
-        ItemDto.BookingShortDto shortDto = new ItemDto.BookingShortDto(1L, 1L);
-        itemDto.setLastBooking(shortDto);
-        assertEquals("item", itemDto.getName());
-        assertEquals(1L, itemDto.getLastBooking().getId());
+        User user = new User(1L, "Name", "e@m.com");
+        assertNotNull(user.getId());
+        assertNotNull(user.getName());
+        assertNotNull(user.getEmail());
+        assertNotNull(user.toString());
 
-        BookingOutDto outDto = new BookingOutDto();
-        outDto.setId(10L);
-        assertEquals(10L, outDto.getId());
+        UserDto userDto = new UserDto(1L, "Name", "e@m.com");
+        assertNotNull(userDto.getId());
+        assertNotNull(userDto.getName());
+        assertNotNull(userDto.getEmail());
+        assertNotNull(userDto.toString());
 
-        ItemRequestDto reqDto = new ItemRequestDto(1L, "desc", null, List.of());
-        assertEquals("desc", reqDto.getDescription());
+        ItemRequest req = new ItemRequest(1L, "desc", user, now);
+        assertNotNull(req.getId());
+        assertNotNull(req.getDescription());
+        assertNotNull(req.getRequestor());
+        assertNotNull(req.getCreated());
+        assertNotNull(req.toString());
 
-        UserDto userDto = new UserDto(1L, "N", "e@m.com");
-        assertEquals("N", userDto.getName());
+        ItemRequestDto reqDto = new ItemRequestDto(1L, "desc", now, List.of());
+        assertNotNull(reqDto.getId());
+        assertNotNull(reqDto.getDescription());
+        assertNotNull(reqDto.getCreated());
+        assertNotNull(reqDto.getItems());
+        assertNotNull(reqDto.toString());
+
+        Item item = new Item(1L, "Name", "Desc", true, user, req);
+        assertNotNull(item.getId());
+        assertNotNull(item.getName());
+        assertNotNull(item.getDescription());
+        assertNotNull(item.getAvailable());
+        assertNotNull(item.getOwner());
+        assertNotNull(item.getRequest());
+        assertNotNull(item.toString());
+
+        ItemDto.BookingShortDto shortDto = new ItemDto.BookingShortDto(1L, 2L);
+        ItemDto itemDto = new ItemDto(1L, "Name", "Desc", true, 1L, shortDto, shortDto, List.of());
+        assertNotNull(itemDto.getId());
+        assertNotNull(itemDto.getName());
+        assertNotNull(itemDto.getDescription());
+        assertNotNull(itemDto.getAvailable());
+        assertNotNull(itemDto.getRequestId());
+        assertNotNull(itemDto.getLastBooking());
+        assertNotNull(itemDto.getNextBooking());
+        assertNotNull(itemDto.getComments());
+        assertNotNull(itemDto.toString());
+
+        Comment comment = new Comment(1L, "text", item, user, now);
+        assertNotNull(comment.getId());
+        assertNotNull(comment.getText());
+        assertNotNull(comment.getItem());
+        assertNotNull(comment.getAuthor());
+        assertNotNull(comment.getCreated());
+        assertNotNull(comment.toString());
+
+        CommentDto commentDto = new CommentDto(1L, "text", "author", now);
+        assertNotNull(commentDto.getId());
+        assertNotNull(commentDto.getText());
+        assertNotNull(commentDto.getAuthorName());
+        assertNotNull(commentDto.getCreated());
+        assertNotNull(commentDto.toString());
+
+        Booking booking = new Booking(1L, now, now, item, user, BookingStatus.WAITING);
+        assertNotNull(booking.getId());
+        assertNotNull(booking.getStart());
+        assertNotNull(booking.getEnd());
+        assertNotNull(booking.getItem());
+        assertNotNull(booking.getBooker());
+        assertNotNull(booking.getStatus());
+        assertNotNull(booking.toString());
+
+        BookingDto bookingDto = new BookingDto(1L, now, now);
+        assertNotNull(bookingDto.getItemId());
+        assertNotNull(bookingDto.getStart());
+        assertNotNull(bookingDto.getEnd());
+        assertNotNull(bookingDto.toString());
+
+        BookingOutDto bookingOutDto = new BookingOutDto(1L, now, now, BookingStatus.WAITING, userDto, itemDto);
+        assertNotNull(bookingOutDto.getId());
+        assertNotNull(bookingOutDto.getStart());
+        assertNotNull(bookingOutDto.getEnd());
+        assertNotNull(bookingOutDto.getStatus());
+        assertNotNull(bookingOutDto.getBooker());
+        assertNotNull(bookingOutDto.getItem());
+        assertNotNull(bookingOutDto.toString());
     }
 }
