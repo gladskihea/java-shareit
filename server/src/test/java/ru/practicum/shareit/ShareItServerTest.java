4cd721131d90afe88b5b8926a5c55fad128f1122
Evgeniy <gladskih07@yandex.ru>
2026-07-11 14:43:43 +0300

    покртытие тестами

--- server/src/test/java/ru/practicum/shareit/ShareItServerTest.java
+++ server/src/test/java/ru/practicum/shareit/ShareItServerTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 class ShareItServerTest {
     @Test
     void mainTest() {
-        ShareItServer.main(new String[] {"--spring.profiles.active=test", "--server.port=0"});
+        ShareItServer.main(new String[] {
+                "--server.port=0",
+                "--spring.datasource.url=jdbc:h2:mem:shareit;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE",
+                "--spring.datasource.driver-class-name=org.h2.Driver",
+                "--spring.sql.init.mode=always"
+        });
     }
 }
