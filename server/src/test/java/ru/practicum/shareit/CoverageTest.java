d664c4f3c6cea379a81684cf04bbb9885cdf3129
Evgeniy <gladskih07@yandex.ru>
2026-07-12 14:10:04 +0300

    покрытие тестами

--- server/src/test/java/ru/practicum/shareit/CoverageTest.java
+++ server/src/test/java/ru/practicum/shareit/CoverageTest.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit;
 import org.junit.jupiter.api.Test;
 import ru.practicum.shareit.booking.dto.BookingMapper;
 import ru.practicum.shareit.item.ItemMapper;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.UserMapper;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
